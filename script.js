fbc628037a36ff098e08a48c83a4d43b50e6fb5c
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-02-10 11:12:05 +0000

    Testing Javascript functionality

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const oneBtn = document.getElementById('one');
+const two = document.getElementById('two');
+
+
+oneBtn.addEventListener('click', function() {
+    document.getElementById('display').innerHTML = '1';
+});
+
+two.addEventListener('click', function() {
+    document.getElementById('display').innerHTML = '2';
+});
+
+
