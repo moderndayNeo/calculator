0bf9ae9f0f24b98567ecfc4daef2a9ac66910c90
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-02-11 11:48:58 +0000

    Substituted unnecessary eventListeners for a getElementsByClassName function

--- script.js
+++ script.js
@@ -1,4 +1,64 @@
+const output = document.getElementById('output');
+// Screen output is a string, to be evaluated each time the user clicks 'equals'
+// On clicking each button, the relevant value 'e.g. 1' is added to the string
+// I have applied the class '.blackButtons' to buttons that ADD to the string. This is all buttons besides Clear, Delete and Equals.
+const blackButtons = document.getElementsByClassName('blackButtons');
+
+for (const value of blackButtons) {
+    value.addEventListener('click', function() {
+        output.innerHTML += this.innerHTML;
+    });
+}
+
+
+
+
+
+
+
+/*
+function maximumStringLength() {
+    
+}
+*/
+
+/*
+let maximumStringLength = output.innerHTML.maxLength;
+maximumStringLength = 2;
+console.log(maximumStringLength);
+let maxSize = output.innerHTML.size;
+maxSize = 2;
+console.log(maxSize);
+
+each button onclick, output += button's innerHTML
+
+*/
+
+
+
+clear.addEventListener('click', function() {
+    output.innerHTML = '';
+});
+// Return to empty string
+
+/*  Remove LAST typed character
+deleteBtn.addEventListener('click', function() {
+    output.innerHTML += '2';
+});
+*/
+
+/*  Evaluate (eval) the string
+equals.addEventListener('click', function() {
+    output.innerHTML += '2';
+});
+*/
+
+// Output overflows if too many numbers are typed. Set a max length for the output string
+// If string.length is greater than 20, throw an error
+
+
 // Declaring constants for all buttons
+/*
 const one = document.getElementById('one');
 const two = document.getElementById('two');
 const three = document.getElementById('three');
@@ -18,98 +78,4 @@ const divide = document.getElementById('divide');
 const add = document.getElementById('add');
 const subtract = document.getElementById('subtract');
 const equals = document.getElementById('equals');
-
-const output = document.getElementById('output');
-
-// Screen output is a string, to be evaluated each time the user clicks 'equals'
-// On clicking each button, the relevant value 'e.g. 1' is added to the string
-
-one.addEventListener('click', function() {
-   output.innerHTML += '1';
-});
-
-two.addEventListener('click', function() {
-    output.innerHTML += '2';
-});
-
-three.addEventListener('click', function() {
-    output.innerHTML += '3';
-});
-
-four.addEventListener('click', function() {
-    output.innerHTML += '4';
-});
-
-five.addEventListener('click', function() {
-    output.innerHTML += '5';
-});
-
-six.addEventListener('click', function() {
-    output.innerHTML += '6';
-});
-
-seven.addEventListener('click', function() {
-    output.innerHTML += '7';
-});
-
-eight.addEventListener('click', function() {
-    output.innerHTML += '8';
-});
-
-nine.addEventListener('click', function() {
-    output.innerHTML += '9';
-});
-
-zero.addEventListener('click', function() {
-    output.innerHTML += '0';
-});
-
-decimal.addEventListener('click', function() {
-    output.innerHTML += '.';
-});
-
-
-clear.addEventListener('click', function() {
-    output.innerHTML = '';
-});
-// Return to empty string
-
-/*  Remove LAST typed character
-deleteBtn.addEventListener('click', function() {
-    output.innerHTML += '2';
-});
 */
-
-multiply.addEventListener('click', function() {
-    output.innerHTML += ' * ';
-}); // Multiply
-
-divide.addEventListener('click', function() {
-    output.innerHTML += ' / ';
-});
-
-add.addEventListener('click', function() {
-    output.innerHTML += ' + ';
-});
-
-subtract.addEventListener('click', function() {
-    output.innerHTML += ' - ';
-    console.log(output.innerHTML.length);
-});
-
-/*  Evaluate (eval) the string
-equals.addEventListener('click', function() {
-    output.innerHTML += '2';
-});
-*/
-
-// Output overflows if too many numbers are typed. Set a max length for the output string
-// If string.length is greater than 20, throw an error
-
-
-function limitOutputLength() {
-    if (output.innerHTML.length > 20) {
-        output.innerHTML = 'Error';
-    }
-}
-
