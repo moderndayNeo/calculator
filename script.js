4d8cd02f447dc8dec9d5974e77ec3a040c2b113c
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-02-11 14:00:41 +0000

    Setting functionality for non-numeric buttons

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const output = document.getElementById('output');
+const multiply = document.getElementById('multiply');
+const deleteBtn = document.getElementById('delete');
+const equals = document.getElementById('equals');
+
 /*
 const displayWidth = document.getElementById('display').scrollWidth;
 const outputWidth = output.scrollWidth;
+// Output overflows if too many numbers are typed. Set a max length for the output string
+// If string.length is greater than 20, throw an error or return
 */
 
 // Screen output is a string, to be evaluated each time the user clicks 'equals'
@@ -18,37 +24,35 @@ for (const value of blackButtons) {
     });
 }
 
-
 /*
-addEventListener 
-If output.innerHTML.length > 20; return
+multiply.addEventListener('click', function() {
+    output.innerHTML = output.innerHTML.slice(0,-3);
+    output.innerHTML += ' * ';
+});
 */
 
-
-
+// Click the 'C' (Clear) button, return to an empty screen
 clear.addEventListener('click', function() {
     output.innerHTML = '';
 });
-// Return to empty string
 
-/*  Remove LAST typed character
+// Click the DEL (Delete) button, remove the last typed character
 deleteBtn.addEventListener('click', function() {
-    output.innerHTML += '2';
+   output.innerHTML = output.innerHTML.slice(0,-1);
 });
-*/
 
-/*  Evaluate (eval) the string
+// Click the equals button, evaluate the string
 equals.addEventListener('click', function() {
-    output.innerHTML += '2';
+    output.innerHTML = eval(output.innerHTML);
+    // Regex evaluation. In the string swap out instances of 
+    // ' x + for '*'
+    // and instances of 'divideSign' for /
 });
-*/
-
-// Output overflows if too many numbers are typed. Set a max length for the output string
-// If string.length is greater than 20, throw an error
 
 
-// Declaring constants for all buttons
+
 /*
+// Declaring constants for all buttons
 const one = document.getElementById('one');
 const two = document.getElementById('two');
 const three = document.getElementById('three');
@@ -62,8 +66,6 @@ const zero = document.getElementById('zero');
 const decimal = document.getElementById('decimal');
 
 const clear = document.getElementById('clear');
-const deleteBtn = document.getElementById('delete');
-const multiply = document.getElementById('multiply');
 const divide = document.getElementById('divide');
 const add = document.getElementById('add');
 const subtract = document.getElementById('subtract');
