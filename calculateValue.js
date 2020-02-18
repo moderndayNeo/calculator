9af0d7e8b6eaaf6f18614e45af6aa58a67745710
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-02-18 22:01:08 +0000

    Refactor: change to number methods instead of eval Eval method poses risks. I refactored frontend to produce string of format 'leftValue,operator,rightValue' which is sent to a conversion & calculation function.

---
+++ calculateValue.js
@@ -0,0 +1,14 @@
+function calculateFinalValue(leftValue, operator, rightValue) {
+    // operators are strings. leftValue and rightValue are numbers
+    // code as follow:  if (operator === '*') { return leftValue * rightValue }...etc..
+    // calculateValue will receive a string
+    // string will look like 'numberoperatornumber'
+    // '.95*17
+    // '3.5+-8'
+    // '777/432'
+    // '.634*-.289'
+
+
+
+
+}
--- script.js
+++ script.js
@@ -6,13 +6,13 @@ const divide = document.getElementById('divide');
 
 // Screen output is a string, to be evaluated each time the user clicks 'equals'
 // On clicking a button, the relevant value 'e.g. 1' is added to the string
-// I have applied the class '.blackButtons' to buttons that ADD to the string. This is all buttons besides Clear, Delete and Equals.
-const blackButtons = document.getElementsByClassName('blackButtons');
+// I have applied the class '.numbersAndDecimal' to buttons that ADD to the string. This is all buttons besides Clear, Delete and Equals.
+const numbersAndDecimal = document.getElementsByClassName('numbersAndDecimal');
 
 const checkForOperator = /[+-/*]\s$/g; // Check if the last value entered was an operator
 const checkForMultiplyOrDivide = /\*|\//g;  // Check if the value entered is 'multiply' or 'divide'
 
-for (const value of blackButtons) {
+for (const value of numbersAndDecimal) {
     value.addEventListener('click', function() { 
             if (checkForOperator.test(output.innerHTML) === true) { // Check if last input was an operator
                 if (checkForMultiplyOrDivide.test(value.innerHTML) === false) { // Check if user is trying to follow an operator with a divide/multiply
@@ -36,6 +36,74 @@ deleteBtn.addEventListener('click', function() {
     } output.innerHTML = output.innerHTML.slice(0,-1);
 });
 
+/* breakdown the back-end inputs into leftValue, operator, rightValue.
+Go through the user cases (user types this, user types that etc.)
+For each test case, either send values to backend or throw an error
+
+
+My default state for the screen is named displayZero.
+displayZero is displayed when the user first loads the calculator, and when the user
+clicks Clear.
+
+
+Screen begins at displayZero.
+1) User begins by typing out a number - The number is a string
+
+Then user types an a) operator, b) decimal
+
+1a) User follows the number with an operator
+Two things happen:
+1. the zero is removed, the screen output is a string, and shows what's been typed
+2. The digits typed so far (number string) gets converted from a string to a number,
+and assigned to leftValue
+
+The user has pressed a number and an operator already. User can now press number,decimal,operator
+If user now presses a number:   the operator is assigned to operator, previous operator
+gets knocked off the old string.
+If user presses another operator:   new operator is assigned to operator (unless
+    the new operator is a minus, in which case previous operator is assigned to operator, minus becomes
+    the beginning of rightValue).
+
+If user presses a decimal (so far we've had number, operator, decimal),decimal
+becomes the beginning of rightValue.
+After decimal user can press number, operator, or decimal.
+If user presses a number, it continues to concatenate to rightValue
+If user presses an operator, return
+If user presses another decimal, return
+
+
+
+
+2) User begins by typing an operator
+a) If at displayZero the user types an operator, zero is assigned to leftValue,
+operator is assigned to operator.
+User can then type a number, operator or decimal.
+If user types a number, it becomes the beginning of rightValue
+If user types an operator, it is assigned to operator (unless it is a minus, in which case
+    it becomes the beginning of rightValue)
+
+If user types a decimal (so far zero, operator, decimal), it becomes beginning of leftValue.
+If user follows with a number, it becomes rightValue
+If user tries to follow with an operator or decimal, return.
+
+
+3) User begins by typing a decimal. It becomes part of the string for leftValue.
+User then types a number, operator or decimal.
+a) User types a number, it continues to concatenate to string
+    User has now typed a decimal and a number. User can now type an operator or a decimal.
+    If user types an operator, continue to concatenate to string
+    If user types a decimal, return
+
+b) User types an operator, return
+c) User types a decimal, return
+
+
+
+backend
+
+*/
+
+
 function checkLastFourDigits(num) {
     return num === parseInt(num) ? num : num.toFixed(4);
 }
@@ -50,6 +118,8 @@ equals.addEventListener('click', function() {
     const shortenedCalculatedValue = checkLastFourDigits(rawCalculatedValue);
 
     output.innerHTML = shortenedCalculatedValue;
+
+    calculateFinalValue(leftValue, operator, rightValue)
 });
 
 
